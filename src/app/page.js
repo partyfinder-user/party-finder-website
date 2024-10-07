7aa43eb90089e91553363072b46bda7d33e6d9b7
partyfinder-user <developers@partyfinder.it>
2024-10-07 13:01:46 +0200

    update event default list

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,70 @@ import Image from "next/image";
 
 import Main from "@/components/Layouts/Main";
 import BgGlassmorphism from "@/components/Helpers/BgGlassmorphism";
-
-import imageStockLocale from "/public/stock/LogoAryas.jpg";
 import { HeartIcon } from "@heroicons/react/24/outline";
+
+const locals = [
+  {
+    id: 2,
+    name: "Villa Bonin",
+    image: "/stock/local-2.jpg",
+  },
+  {
+    id: 3,
+    name: "Hollywood",
+    image: "/stock/local-3.jpg",
+  },
+  {
+    id: 4,
+    name: "Amnesia",
+    image: "/stock/local-4.jpg",
+  },
+  {
+    id: 5,
+    name: "Mr. Charlie",
+    image: "/stock/local-5.png",
+  },
+  {
+    id: 6,
+    name: "Pacha Ibiza",
+    image: "/stock/local-6.jpg",
+  },
+  {
+    id: 1,
+    name: "Club Papaya",
+    image: "/stock/local-1.jpg",
+  },
+  {
+    id: 7,
+    name: "Baia Imperiale",
+    image: "/stock/local-7.jpg",
+  },
+  {
+    id: 8,
+    name: "Fabrique",
+    image: "/stock/local-8.jpg",
+  },
+  {
+    id: 9,
+    name: "Samsara Beach",
+    image: "/stock/local-1.jpg",
+  },
+  {
+    id: 10,
+    name: "Il Muretto",
+    image: "/stock/local-2.jpg",
+  },
+  {
+    id: 11,
+    name: "Mamacita Club",
+    image: "/stock/local-3.jpg",
+  },
+  {
+    id: 12,
+    name: "Cocoricò",
+    image: "/stock/local-4.jpg",
+  },
+];
 
 const musicGenres = [
   "House",
@@ -206,24 +267,26 @@ export default function Home() {
           <section>
             <h2 className="text-lg font-semibold mb-2">Locali in evidenza</h2>
             <div className="relative snap-x mx-auto snap-mandatory overflow-x-scroll overflow-y-hidden scrollbar-hide">
-              <div className="w-full flex flex-row justify-between gap-4">
-                {Array.from({ length: 12 }).map((_, index) => (
+              <div className="w-full flex flex-row justify-between gap-2">
+                {locals.map((locale) => (
                   <div
-                    key={index}
+                    key={locale.id}
                     className="flex flex-col items-center justify-center gap-3"
                   >
                     <div className="relative w-20 h-20 rounded-full p-1 bg-gradient-to-tr from-yellow-500 via-[#b592ff] to-[#5200FF]">
                       <div className="w-full h-full rounded-full">
                         <Image
-                          src={imageStockLocale}
-                          width="600px"
-                          height="600px"
-                          alt="locandina evento"
+                          src={locale.image}
+                          width={80}
+                          height={80}
+                          alt={locale.name}
                           className="w-full h-full rounded-full object-cover border-gray-800 border"
                         />
                       </div>
                     </div>
-                    <span className="text-sm">Tweet</span>
+                    <span className="text-xs text-white text-center max-w-[100px] whitespace-nowrap overflow-hidden text-ellipsis">
+                      {locale.name}
+                    </span>
                   </div>
                 ))}
               </div>
@@ -236,13 +299,13 @@ export default function Home() {
               Generi più seguiti
             </h2>
             <div className="relative snap-x mx-auto snap-mandatory overflow-x-scroll overflow-y-hidden scrollbar-hide">
-              <div className="w-full flex flex-row gap-4">
+              <div className="w-full flex flex-row gap-2">
                 {musicGenres.map((genere, idx) => (
                   <div
                     key={idx}
                     className="flex flex-col items-center justify-center gap-3"
                   >
-                    <div className="text-sm px-4 py-2 bg-background-500/70 border border-background-400 rounded-full text-white whitespace-nowrap">
+                    <div className="text-sm px-5 py-2 bg-background-500/70 border border-background-400 rounded-full text-white whitespace-nowrap">
                       {genere}
                     </div>
                   </div>
@@ -261,7 +324,7 @@ export default function Home() {
                 {topEvents.map((event, idx) => (
                   <div
                     key={idx}
-                    className="flex flex-col justify-between gap-2 bg-background-500/40 rounded-lg shadow-lg min-w-[300px] overflow-hidden"
+                    className="flex flex-col justify-between gap-2 bg-background-500/60 border border-background-500 rounded-lg shadow-lg min-w-[300px] overflow-hidden"
                   >
                     <div className="relative">
                       <Image
@@ -276,7 +339,7 @@ export default function Home() {
                         <HeartIcon className="w-6 h-6 text-white" />
                       </div>
 
-                      <div className="absolute top-0 right-0 bg-background-800/80 backdrop-blur-sm text-neon text-md px-3.5 py-1.5 rounded-bl-lg">
+                      <div className="absolute top-0 right-0 bg-background-500/60 border border-background-500 backdrop-blur-sm text-neon text-md px-3.5 py-1.5 rounded-bl-lg">
                         {event.venue}
                       </div>
 
@@ -285,11 +348,11 @@ export default function Home() {
                           {event.title}
                         </span>
 
-                        <div className="mt-auto flex flex-col">
+                        <div className="mt-auto flex flex-col gap-1">
                           <span className="text-xs text-accent-400">
                             {event.date}
                           </span>
-                          <span className="text-xs text-gray-400">
+                          <span className="text-xs text-background-200">
                             {event.location}
                           </span>
                         </div>
@@ -301,7 +364,7 @@ export default function Home() {
             </div>
           </section>
 
-          <section className="pb-4">
+          <section className="pb-6">
             <h2 className="text-lg font-semibold mb-2 text-white">
               Party di fine estate
             </h2>
@@ -310,7 +373,7 @@ export default function Home() {
                 {otherEvents.map((event, idx) => (
                   <div
                     key={idx}
-                    className="flex flex-col justify-between gap-2 bg-background-500/40 rounded-lg shadow-lg min-w-[300px] overflow-hidden"
+                    className="flex flex-col justify-between gap-2 bg-background-500/60 border border-background-500 rounded-lg shadow-lg min-w-[300px] overflow-hidden"
                   >
                     <div className="relative">
                       <Image
@@ -334,11 +397,11 @@ export default function Home() {
                           {event.title}
                         </span>
 
-                        <div className="mt-auto flex flex-col">
+                        <div className="mt-auto flex flex-col gap-1">
                           <span className="text-xs text-accent-400">
                             {event.date}
                           </span>
-                          <span className="text-xs text-gray-400">
+                          <span className="text-xs text-background-200">
                             {event.location}
                           </span>
                         </div>
@@ -352,36 +415,40 @@ export default function Home() {
 
           <section>
             <h2 className="text-lg font-semibold mb-2 text-white">
-              Altri Eventi in Arrivo
+              Altri eventi in programma
             </h2>
             <div className="w-full flex flex-col gap-4">
               {events.map((event, idx) => (
                 <div
                   key={idx}
-                  className="flex flex-row items-center gap-4 p-4 bg-gray-700/50 border border-gray-600 rounded-lg shadow-sm hover:bg-gray-700 transition-all duration-300"
+                  className="flex flex-row items-stretch bg-background-500/60 border border-background-500 rounded-lg shadow-sm hover:bg-background-700 transition-all duration-300 overflow-hidden"
                 >
-                  {/* Immagine piccola in secondo piano */}
                   <div className="flex-shrink-0">
                     <Image
                       src={event.image}
-                      width={80}
-                      height={80}
+                      width={170}
+                      height={200}
                       alt={event.title}
-                      className="rounded-lg object-cover"
+                      className="object-cover h-full rounded-l-lg"
                     />
                   </div>
 
-                  {/* Dettagli dell'evento */}
-                  <div className="flex flex-col">
-                    <span className="text-sm font-semibold text-gray-300">
-                      {event.title} · {event.venue}
-                    </span>
-                    <span className="text-xs text-accent-400">
-                      {event.date}
-                    </span>
-                    <span className="text-xs text-gray-400">
-                      {event.location}
-                    </span>
+                  <div className="flex flex-col justify-between px-4 py-2 flex-grow gap-2">
+                    <div>
+                      <span className="text-lg font-semibold text-white block">
+                        {event.title}
+                      </span>
+                      <span className="text-accent-500">{event.venue}</span>
+                    </div>
+
+                    <div className="flex flex-col">
+                      <span className="text-xs text-white mb-1">
+                        {event.date}
+                      </span>
+                      <span className="text-xs text-background-200">
+                        {event.location}
+                      </span>
+                    </div>
                   </div>
                 </div>
               ))}
