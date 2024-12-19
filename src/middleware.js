c6753af4cda860d98a4c7064e702516b73574158
partyfinder-user <developers@partyfinder.it>
2024-12-19 15:50:28 +0100

    fix env

--- src/middleware.js
+++ src/middleware.js
@@ -9,8 +9,7 @@ export async function middleware(req) {
   const ipList = ipWhiteList.split(',');
 
   const detectedIp = req.ip;
-  console.log(detectedIp);
-  console.log(process.env.ENV);
+  console.log('ENV', process.env.ENV);
   
   if (process.env.ENV === 'staging' && !ipList?.includes(detectedIp)) {
     if (!isAuthenticated(req)) {
